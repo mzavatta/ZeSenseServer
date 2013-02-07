1c4bc468b11e348b736f00b1d31d12692a17afc6
Marco Zavatta <zavaboss@gmail.com>
2013-02-07 17:28:11 +0100

    cleanup

---
+++ Android.mk
@@ -0,0 +1,16 @@
+# Marco Zavatta
+# TELECOM Bretagne
+# Android.mk build file for libcoap by Olaf Bergmann
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := zesensecoap
+LOCAL_SRC_FILES := ze_coap_reqbuf.c ze_coap_resources.c ze_coap_server_core.c ze_coap_server_root.c ze_sm_reqbuf.c ze_streaming_manager.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../libcoap-3.0.0-android
+LOCAL_LDLIBS  := -llog -landroid -lEGL -lGLESv1_CM
+LOCAL_CFLAGS :=  -Wall -Wextra -std=c99 -pedantic -g -O2
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+
+include $(BUILD_STATIC_LIBRARY)
